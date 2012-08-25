22d1b2f151471c51e8fbd64407f0550b3daed6d1
itacademy <itacademy@192.168.1.11>
2012-08-25 21:31:35 +0900

    html

---
+++ src/jp/itacademy/sample/web/html.java
@@ -0,0 +1,30 @@
+package jp.itacademy.sample.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class html
+ */
+@WebServlet("/html")
+public class html extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest req,
+			HttpServletResponse res)
+			throws ServletException, IOException {
+	 
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
